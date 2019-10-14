78c9021c2f23a571b4f3843ec73d4ef24b600325
samunico <nicolai.berk@gmail.com>
2019-10-15 00:31:43 +0200

    Decide n of topics, add topics to documents, make plots.

--- LDA.R
+++ LDA.R
@@ -1,7 +1,7 @@
 # LDA
 rm(list = ls())
 usePackage <- function(p) {if (!is.element(p, installed.packages()[,1]))install.packages(p,dep = TRUE, repos = "http://cran.wu.ac.at"); library(p, character.only = TRUE)}
-pkgs <- c('googledrive', 'topicmodels','tidytext', 'dplyr', 'quanteda', 'ggplot2'); for (i in pkgs){usePackage(i)}
+pkgs <- c('googledrive', 'topicmodels','tidytext', 'dplyr', 'quanteda', 'ggplot2', 'ldatuning', 'tidyr'); for (i in pkgs){usePackage(i)}
 
 ## download/update data
 setwd("~/GitHub/samunico/Apart/data")
@@ -13,7 +13,7 @@ drive_download(file = fn, overwrite = T)
 dail30 <- read.csv('30th_Dail_incl_positions.csv')
 
 ### subset for smoother workflow (get rid of later)
-dail30 <- dail30[1:100,]
+dail30 <- dail30[sample(nrow(dail30), size = 1000, replace = F),]
 
 ## transform into document-term-matrix
 dail30_dtm <- dail30$speech %>%
@@ -22,15 +22,33 @@ dail30_dtm <- dail30$speech %>%
       tolower = T,
       stem = T,
       remove = stopwords('english'),
-      # remove frequent words
-      remove_punct = T)
+      remove_punct = T) %>%
+  dfm_trim(max_docfreq = 0.1, docfreq_type = "prop")
 
-dail30_dtm <- dail30_dtm[rowSums(dail30_dtm) > 20, ]
+dail30_dtm <- dail30_dtm[rowSums(dail30_dtm) > 10, ]
+
+
+
+## get perfect n of topics
+result <- FindTopicsNumber(
+  dail30_dtm,
+  topics = seq(from = 2, to = 50, by = 1),
+  metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
+  method = "Gibbs",
+  control = list(seed = 77),
+  mc.cores = 2L,
+  verbose = TRUE
+)
+
+#### elbow plot
+FindTopicsNumber_plot(result)
+
+ggsave('plots/LDA_bestfit.png', width = 400, height = 150, units = 'mm')
 
 
 
 ## lda
-dail30_lda <- LDA(dail30_dtm, k = 30, control = list(seed = 1234))
+dail30_lda <- LDA(dail30_dtm, k = 37, control = list(seed = 1234))
 
 ## generate term probabilities per topic
 dail30_topics <- tidy(dail30_lda, matrix = "beta")
@@ -47,11 +65,25 @@ dail30_top_terms %>%
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
   coord_flip() +
-  scale_x_reordered()
+  scale_x_reordered() + 
+  theme(text = element_text(size=20))
 
 ggsave('plots/LDA_topwords.png', width = 1000, height = 500, units = 'mm')
 
+
+
 ## extract probability of each topic for each document
+dail30_documents <- tidy(dail30_lda, matrix = "gamma")
 
+### rows to columns
+dail30_wide <- spread(dail30_documents, topic, gamma)
 
-## add topics to initial dataframe, based on speechID's
+### sort
+dail30 <- dail30[order(dail30$speechID), ]
+dail30_wide <- dail30_wide[order(dail30_wide$document), ]
+dail30_wide$document <- as.integer(dail30_wide$document)
+dail30 <- dail30[dail30$speechID %in% dail30_wide$document, ] # remove dropped obs (due to short speech)
+
+### add topics to initial dataframe, based on speechID's
+dail30_topics <- cbind(dail30, dail30_wide)
+write.csv(dail30_topics, file = "dail30_topics.csv")
