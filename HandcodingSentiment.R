22c6dd64ed7d33bfa072bbe719c1506348c85080
samunico <nicolai.berk@gmail.com>
2019-11-16 21:12:53 +0100

    Adjust for newer data, add speech output.

--- HandcodingSentiment.R
+++ HandcodingSentiment.R
@@ -8,17 +8,17 @@
 #
 # 1. Preparation -----------------------------------------------------
 # __Loading Packages -------------------------------------------------
-setwd("~/GitHub/samunico/Apart/data")
+setwd("~/GitHub/samunico/Apart")
 rm(list=ls())
 usePackage <- function(p) {if (!is.element(p, installed.packages()[,1]))install.packages(p,dep = TRUE, repos = "http://cran.wu.ac.at"); library(p, character.only = TRUE)}
 for (i in c('googledrive', 'data.table', 'dplyr')){usePackage(i)}
 
 # __global vars  -----------------------------------------------------
-subsample = 1000 # define sample size, 'full' for full sample
+subsample = 'full' # define sample size, 'full' for full sample
 
 # __Loading Data -----------------------------------------------------
 ## include code to download random sample 
-df <- fread("df_window_30th_dail.csv", encoding = "UTF-8")
+df <- fread("sample_nico.csv", encoding = "UTF-8")
 
 if (subsample != 'full'){
   df <- sample_n(df, subsample)
@@ -27,8 +27,13 @@ if (subsample != 'full'){
 df$handcoding <- NA
 
 for (i in (1:nrow(df))){
-  print(df$window[i])
-  print("IS THE SENTIMENT IN THIS TEXT NEGATIVE [1], NEUTRAL [2], OR POSITIVE [3]?")
+  cat("Window:\n")
+  cat(df$window[i])
+  cat("Speech:\n")
+  cat(df$speech[i])
+  cat("IS THE SENTIMENT IN THIS TEXT NEGATIVE [1], NEUTRAL [2], OR POSITIVE [3]?")
   value <- readline(prompt = "Value: ")
   if(value == "quit"){break}else{(df$handcoding[i] <- as.numeric(value)-2)}
 }
+
+write.csv(df, file = "nico_sample.csv", fileEncoding = "UTF-8")
