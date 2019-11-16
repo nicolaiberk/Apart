8182a0bfeca4631c8cb1d126e3710403a9cf15bd
pmmendoza <medihelp.1e@gmail.com>
2019-11-16 16:49:19 +0100

    updates

--- newentitiesdict_30th_dail.R
+++ newentitiesdict_30th_dail.R
@@ -3,79 +3,119 @@
 # Author: Philipp M.
 # Date: Fri Nov 15 12:46:26 2019
 # 0. Content ------------------------------------------------------------
-#   1. Load full set of 
-#
-# 1. Preparation --------------------------------------------------------
-# __Loading Packages -------------------------------------------------
+#   1. Preparation
+#   2. Creating Entity Dictionary
+#   
+#   
+# 1. Preparation -------------------------------------------------------
+# __Loading Packages ---------------------------------------------------
 usePackage <- function(p) {if (!is.element(p, installed.packages()[,1]))install.packages(p,dep = TRUE, repos = "http://cran.wu.ac.at"); library(p, character.only = TRUE)}
-for (i in c('beepr', 'tidyverse', 'rio', 'tidylog', 'skimr', 'clipr',
+for (i in c('beepr', 'tidyverse', 'rio', 'tidylog', 'clipr',
             'quanteda', 'readtext', 'Hmisc', 'rio',
             'googledrive', 'readtext', 'data.table', 'stringr', 'qdap')){
   usePackage(i)}
+# detach("package:skimr", unload=TRUE)
+
+
+# __global vars  -----------------------------------------------------
+# 1) original dataset fromat Harvard dataverse
+# If you want to start from the original Harvard Dataverse dataset you'll have to download it manually first & paste it into the current file directory: https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/6MZN76/CRUNF0
+
+DATASRC <-  2 # set to x to start from: 
+              #   1) [NOT WORKING] Harvard Dataverse compressed file ("Dail_debates_1919-2013.tar.gz")
+              #   2) Google Drive compressed file
+              #   3) Ready-made local dataset
+UPLOAD <-  T  # Upload Entity dictionary to drive?
+PLOT <- F     # Plot distribution of speeches across legislative periods
+
 
 # __Loading Data -----------------------------------------------------
-
-# Get a date overview of Dáils:
+## 1. Data frame of Dáil Periods & speakers
 daildict <- import("DailPeriods.csv")
 daildict <- daildict %>% mutate(
   begin = begin %>% as.Date("%d/%m/%Y"),
   end = end %>% as.Date("%d/%m/%Y")
 )
 
-# Get the universe of speeches
-# Get Data from: 
-# 1) Website (original)  TRUE
-# 2) local (processed)  FALSE
-downl <- TRUE
-
-if (downl){
-  # These lines downlad the original data from the website; unpack them; and read them into the program
-  # Following line not yet working, somehow I can unpack the manually downloaded file, but not the one downloaded via R
-  # downloading file
-  download.file(url="https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/6MZN76/CRUNF0",
-                destfile = "Dail_debates_1919-2013.tar.gz") # https://stat.ethz.ch/R-manual/R-devel/library/utils/html/download.file.html
-  # unpacking the file
-  untar(tarfile = "Dail_debates_1919-2013.tar.gz")
-  list.files()
+## 2.Universe of speeches
+if (DATASRC<3) {
+  if (DATASRC==1) { # From Harvard Dataverse
+    # These lines downlad the original data from the website [not working atm; somehow I can unpack the manually downloaded file, but not the one downloaded via R]; unpack them; and read them into the program
+    
+    # download [not working atm]
+    # download.file(destfile = "Dail_debates_1919-2013.tar.gz", 
+    #               url="https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/6MZN76/CRUNF0") # https://stat.ethz.ch/R-manual/R-devel/library/utils/html/download.file.html
+  }
+  if (DATASRC==2) { # From Google Drive
+  file_id <- drive_find("Dail_debates_1919-2013.tar.gz", n_max = 10)$id
+  drive_download(paste0("https://drive.google.com/open?id=", file_id), overwrite = T) 
+  }
+  # unpack
+  untar("Dail_debates_1919-2013.tar.gz"); beep(10)
   
-  # read file 
+  # read 
   df <- fread("Dail_debates_1919-2013.tab", sep="\t", 
               quote="", header=TRUE, showProgress = TRUE, data.table=FALSE, verbose = TRUE)
   
-  # latest recorded speech: 2013-03-28
   # Adding legislative period variable:
-  df$datef <- as.Date(df$date) # making sure that date variable is in date format
-  beep(2)
+    df$datef <- as.Date(df$date); beep(10) # make sure that date variable is in date format
+    # ad end-date as current date for the current period
+    daildict$end[daildict$dail==max(daildict$dail)] <- Sys.Date() %>% as.character()
+    df$legper <- NA
+    for (i in 1:nrow(daildict)){
+      df <- df %>% 
+        mutate(legper = ifelse(datef >= daildict$begin[i] &
+                                 datef <= daildict$end[i], 
+                               daildict$dail[i], legper))
+      }
+    
+    # Plotting the number of speeches against the legislative period
+    if (PLOT) {df %>% ggplot(.,aes(x=legper)) + 
+      geom_histogram(bins = max(df$legper,na.rm=T)) + 
+      scale_x_continuous(breaks = 1:max(daildict$dail))}
   
-  daildict$end[daildict$dail==max(daildict$dail)] <- Sys.Date() %>% as.character()
-  df$legper <- NA
-  i <- 30
-  for (i in 1:nrow(daildict)){
-    df <- df %>% mutate(legper = ifelse(datef >= daildict$begin[i] & datef <= daildict$end[i], daildict$dail[i], legper))}
-  
-  # Plotting the number of speeches against the legislative period
-  df %>% ggplot(.,aes(x=legper)) + geom_histogram(bins = max(df$legper,na.rm=T)) + scale_x_continuous(breaks = 1:max(daildict$dail))
-  
+  # Save file locally
   export(df, "dail_full.csv")
-} 
+  
+# } else if (DATASRC==3) {
+  df <- fread("dail_full.csv",
+              header=TRUE, showProgress = TRUE, data.table=FALSE, verbose = TRUE, encoding = "UTF-8")
+  # }
 
-df <- fread("dail_full.csv",
-            header=TRUE, showProgress = TRUE, data.table=FALSE, verbose = TRUE, encoding = "UTF-8")
-# Creating a dictionary ---------------------
-temp$member_name %>% word(1) %>% unique() %>% write_clip()
 
+# Creating an entity dictionary ---------------------
+# Subselect relevant columns
+temp <- df %>% select("memberID", "member_name", "legper", "party_name") %>% distinct() %>% as_tibble()
 
-temp <- df %>% select("memberID", "member_name", "legper") %>% distinct() %>% as_tibble()
+# find formal prefixes of names:
+# temp$member_name %>% word(1) %>% table() %>% sort(decreasing = T)
+
+# check which nicknames there are
+nick <- str_extract(temp$member_name, "\\((.*?)\\)") %>% 
+  table() %>% sort(decreasing = T) %>% names()
+nonick <- c("(Snr.)", "(Deceased)", "(Major-General)", "(Jnr.)", "(Limerick West)", "(Senior)", "(The Cope)", "(Captain)", "(Dr.)", "(Major)", "(Dublin South-Central)", "(Longford-Roscommon)", "(Tipperary)", "(Cork City)", "(Resigned)", "(Wexford)", "(Tipperary South)", "(Cork Borough)", "(Cork West)", "(deceased)", "(Dublin West)", "(Edenderry, Offaly)", "(Mayo North)", "(Snr)", NA)
+
+# nonick %>% 
+
+# # # add parties
+# # # add nicknames
+
+# Create cleaned names & surname columns
 temp2 <- temp %>% mutate(
   originalname = member_name, # original name (not clean)
-  fullname = member_name %>% str_remove_all("Count |Mr. |Dr. |Professor |General |Ms. |Countess |Sir |Capt. |Major |Mrs. |Colonel"), 
-    # str_remove_all(as.character(member_name), "\\((.*?)\\)") %>% 
-    #      str_remove_all(" RIP") %>% 
-    #      word(2,-1), # clean full name
-       surname=(str_remove_all(as.character(member_name), "\\((.*?)\\)") %>%
-                  str_remove_all(" RIP") %>% strsplit(split=" ") %>% # clean last name
-                  lapply(function(x) {x[length(x)]}) %>% unlist)
-       ) 
+  fullname = member_name %>% 
+    str_remove_all("Count |Mr. |Dr. |Professor |General |Ms. |Countess |Sir |Capt. |Major |Mrs. |Colonel | RIP"),
+  nickname = ifelse(!str_extract(fullname, "\\((.*?)\\)") %in% nonick, (str_extract(fullname, "\\((.*?)\\)") %>% str_remove_all("\\(|\\)")), NA),
+  fullname = fullname %>% str_remove_all("\\((.*?)\\)"),
+    # word(2,-1), # clean full name
+  surname = (str_remove_all(member_name, "\\((.*?)\\)") %>%
+               str_remove_all(" RIP") %>% strsplit(split=" ") %>% # clean last name
+               lapply(function(x) {x[length(x)]}) %>% unlist)
+       )
+
+
+temp2$fullname %>% unique()
+temp2$nickname %>% unique()
 
 
 temp2 <- temp2 %>% group_by(legper, surname) %>% mutate(shared = ifelse(n()>1,1,0)) %>%  
@@ -145,5 +185,17 @@ drive_upload("entities_30th_dail.csv",
              overwrite = T)
 
 
+
+
+if(upload){
+  # Compress csv file
+  tar("dail_full.csv", compression="gzip")
+  
+  # Update Google Drive File
+  drive_upload("Dail_debates_1919-2013.csv", 
+               path = "~/Internship AffPol in Text/Data/Ireland/",
+               overwrite = T)
+  drive_update()
+}
 
 
