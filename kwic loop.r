90e0f95cfabda283c5750003ddacc6ca68920a22
pmmendoza <medihelp.1e@gmail.com>
2019-12-06 10:54:20 +0100

    Currently final version

--- kwic loop.r
+++ kwic loop.r
@@ -2,39 +2,47 @@
 # Context: APART
 # Author: Philipp M.
 # Date: Sat Nov 16 20:57:36 2019
-# 0. Content ------------------------------------------------------------
+# ---------------------------------------- 0. Content --------------------------------------
 # 1. Preparation
 # 2. Data Cleaning
 # 3. Analyses
 #
 #
-# 1. Preparation --------------------------------------------------------
+# ---------------------------------------- 1. Preparation --------------------------------------
 # __Loading Packages -------------------------------------------------
 rm(list=ls())
 usePackage <- function(p) {if (!is.element(p, installed.packages()[,1]))install.packages(p,dep = TRUE, repos = "http://cran.wu.ac.at"); library(p, character.only = TRUE)}
 for (i in c('beepr', 'tidyverse', 'rio', 'tidylog', 'clipr', 'quanteda', 'data.table', 'rlist', 
-            'parallel', 'doSNOW', 'foreach', 'sentimentr', 'progress')){
+            'parallel', 'doSNOW', 'foreach', 'sentimentr', 'progress', 'inops')){
   usePackage(i)}
 
 # __Loading Data -----------------------------------------------------
 dfo <- data.table::fread("dail_full.csv", encoding = "UTF-8", verbose = T); beep(10)
 entities <- data.table::fread("EntitiesDict.csv", encoding = "UTF-8", verbose = T)
 
+# ---------------------------------------- 2. KWIC --------------------------------------
 # __Global Variables -----------------------------------------------------
 SUBS <- T # Subset or Full
     P <- 1 # change either n or proportion in decimals, e.g. 0.01
-    LP <- 25 # start of data frame in terms of legislative periods / Dails
-    
+    LP <- 13 # start of data frame in terms of legislative periods / Dails
+W <-  50 # Windowsize
+# Upload to drive?
+drive <- F
+
 # __Defining Analysis set -----------------------------------------------------
 if(SUBS){
   if(P>1){df <- dfo %>% tidylog::filter(legper>=LP) %>% tidylog::group_by(legper) %>% dplyr::sample_n(P) %>% dplyr::ungroup() %>% as_tibble()
   } else {df <- dfo %>% tidylog::filter(legper>=LP) %>% tidylog::group_by(legper) %>% dplyr::sample_frac(P) %>% dplyr::ungroup() %>% as_tibble()}
-} else {df <- dfo %>% tidylog::filter(legper>=13); LP <- 1}
+} else {df <- dfo %>% tidylog::filter(legper>=13); LP <- 13}
 df$legper %>% table(useNA = "always") # double check
     rm(dfo)
 
-# __ Prepare Data Slices for parallel -----------------------------------------------------
-    # Slice task for foreach looping: # has to happen within each thingy.
+# __Data cleaning -----------------------------------------------------
+# remove everything with parenthesis:
+system.time(df$speech <- df$speech %>% stringr::str_remove_all("\\([^\\(\\)]*\\)")); beep(10)
+
+# __Setup Parallel Processing -----------------------------------------------------
+## Slice task for foreach looping: # has to happen within each thingy.
 x <- 3000 # size of slice
 lp <- df$legper %>% unique %>% sort()
 for (i in lp){
@@ -47,35 +55,25 @@ for (i in lp){
 }
 # df$randomslice %>% unique %>% sort()    
 
-
 # For subsampling
 # fwrite(df, "n1000_subsample_13-30thdail_raw.csv")
 
-# 2. Data cleaning -----------------------------------------------------
-# remove everything with parenthesis:
-system.time(df$speech <- df$speech %>% stringr::str_remove_all("\\([^\\(\\)]*\\)")); beep(10)
+# __Progress bar -----------------------------------------------------
+  iterations <- df$randomslice %>% max() # Number of iterations
+  pb <- progress_bar$new(
+    format = "legper = :letter [:bar] :elapsed | eta: :eta",
+    total = iterations,   
+    width = 60)
+  progress_letter <- rep(LETTERS[1:5],(iterations/5)+1)[1:iterations]  # token reported in progress bar
+  progress <- function(n){pb$tick(tokens = list(letter = progress_letter[n]))} # allowing progress bar to be used in foreach
+  opts <- list(progress = progress)
 
-
-# 2. KWIC -----------------------------------------------------
-# Set up Progress bar ####
-    numCores<-detectCores(); cl <- makeCluster(numCores);registerDoSNOW(cl)
-    # progress bar ------------------------------------------------------------
-    iterations <- df$randomslice %>% max() # Number of iterations
-    pb <- progress_bar$new(
-      format = "legper = :letter [:bar] :elapsed | eta: :eta",
-      total = iterations,   
-      width = 60)
-    progress_letter <- rep(LETTERS[1:5],(iterations/5)+1)[1:iterations]  # token reported in progress bar
-    progress <- function(n){pb$tick(tokens = list(letter = progress_letter[n]))} # allowing progress bar to be used in foreach
-    opts <- list(progress = progress)
-#####
-
+# __Actual Loop -----------------------------------------------------
 # Ready, Set, Go!!!
-start <- Sys.time()
-# i <- 14
-windowsize <- 50
 # setup parallel backend to use many processors
-# cores <- detectCores(); cl <- makeCluster(cores[1]-1); registerDoParallel(cl)
+numCores<-detectCores(); cl <- makeCluster(numCores);registerDoSNOW(cl)
+start <- Sys.time()
+
 # Actual loop
 df_windows <- foreach (i = 1:iterations,
                        .combine = rbind,
@@ -86,75 +84,128 @@ df_windows <- foreach (i = 1:iterations,
                          dfs <- df %>% filter(randomslice==i) %>% mutate(speechID = speechID %>% as.character)
                          currentdail <- dfs$legper %>% unique 
                          entity <- entities %>% filter(legper==currentdail)
-                         entits <- c(entity$match, entity$alternativematch, entity$party_name) %>% unique #entity$nicknamematch
+                         # Searchterms
+                         entits <- c(entity$match, 
+                                     entity$alternativematch, 
+                                     entity$party_name[!entity$party_name %in% "Independent"]) %>% unique #entity$nicknamematch
                          entits <- entits[!entits %in% c("", "Independent")]
                          dfs %>% corpus(text_field = 'speech', docid_field = 'speechID') %>% 
                            tokens(remove_punct = F) %>% 
-                           kwic(pattern=phrase(entits), window=windowsize, case_insensitive = F) #%>% 
-                           # merge(dfs,., by.x = "speechID", by.y="docname")
+                           kwic(pattern=phrase(entits), window=W, case_insensitive = F) %>% 
+                           merge(dfs,., by.x = "speechID", by.y="docname")
                        }; print(Sys.time()-start); stopCluster(cl); beep(10)
 
 df_windows <- merge(df, df_windows, by.x = "speechID", by.y="docname")
 
 # backup <- df_windows
 
-
-# #####
+# ###
 # tempppp <- .Last.value
 # tempppp <- tempppp %>% mutate(window=paste(pre, pattern, post))
 # tempppp$window[!(tempppp$window %>% grepl("Count Plunkett",.))]
-# #####
+# ###
 
 # Creating unique Window ID
 df_window <- df_windows %>% group_by(speechID) %>% mutate(row = row_number()) %>% ungroup() %>% mutate(window_id = paste(speechID, row, sep="_")) %>% select(-row)
 
 
-# Saving Data -------------------------------
-data.table::fwrite(df_window,"df_windows_full_12-03.csv")
-data.table::fwrite(df_window,"df_windows_full_12-03.csv.gz")
-googledrive::drive_upload(media = "df_windows_full_12-03.csv.gz",
+# ---------------------------------------- Saving Data --------------------------------
+data.table::fwrite(df_window,"df_windows_full_12-05.csv")
+data.table::fwrite(df_window,"df_windows_full_12-05.csv.gz")
+if (drive){
+googledrive::drive_upload(media = "df_windows_full_12-05.csv.gz",
                           path = "~/Internship AffPol in Text/Data/Ireland/",
-                          overwrite = T)
+                          overwrite = T)1;}
 
+## Getting the data
 
-# Visualizing Distribution of window --------
+# __Viz Dist of Windows X Legper --------
 df_window %>% ggplot(.,aes(x=legper)) + 
   geom_histogram(bins = max(df_window$legper,na.rm=T)) + 
   scale_x_continuous(breaks = 1:max(df_window$legper))
 
+# ---------------------------------------- Windows for Sentiment Analysis --------------------------------
 
-# -----
-df_window_sub <- df_window %>%  sample_n(1000)
-# Windows for Sentiment Analysis
+# Load Data from saved file
+# df_window <- fread("df_windows_full_12-03.csv", encoding = "UTF-8")
 
+## Position of Match in Window
 print("Preparation for Sentiment Analyses"); start <- Sys.time()
-df_window_13_sub$window <- paste(df_window_13_sub$pre, df_window_13_sub$keyword, df_window_13_sub$post, sep=" ")
-df_window_13_sub$window_noname <- paste(df_window_13_sub$pre, df_window_13_sub$post, sep=" ")
-corpus_window <- corpus(df_window_13_sub, text_field = 'window_noname')
-Sys.time()-start
+df_window$window <- paste(df_window$pre, df_window$pattern, df_window$post, sep=" ")
+df_window$ntoken_speech <- ntoken(df_window$speech); beep(8)
+df_window$from_token_window <- ifelse(df_window$from<(W+1),
+                                      df_window$from,
+                                      W+1); beep(8)
+df_window$ntokenofmatch <- ntoken(df_window$pattern); beep(8)
+df_window$to_token_window <- df_window$from_token_window+df_window$ntokenofmatch-1
+fwrite(df_window,"df_windows_full_12-05_01.csv"); beep(8)
+# df_window <- fread("df_windows_full_12-03_01.csv", encoding = "UTF-8")
+
+## DOUBLE CHECK HERE
+# for (i in 1:7){(df_window$window[i] %>% str_split(" ") %>% unlist)[df_window$from_token_window[i]:df_window$to_token_window[i]]}
+
+## Sentence-ise the window 
+df_window$window_sentences <- get_sentences(df_window$window); beep(8)
+saveRDS(df_window, "df_windows_full_12-05_02.rds"); beep(8) # because we have a data.table with a list, we better use rds instead of csv!
+
+# df_window <- readRDS("df_windows_full_12-03_02.rds")
+df_window$n_sentences <- sapply(df_window$window_sentences, length); beep(8) # n of sentences in each window
+numCores <- detectCores(); cl <- makeCluster(numCores-1)
+system.time(ntokenssentences <- parLapply(cl, df_window$window_sentences, ntoken)); beep(8)
+ntokenssentences %>% saveRDS("ntokenssentences_full.rds")
+ntokenssentences1 <- lapply(ntokenssentences, as.numeric)
+df_window$n_token_sentence <- lapply(ntokenssentences1,cumsum); beep(8) # cumulative sum of tokens in each sentence of each window
+saveRDS(df_window, "df_windows_full_12-05_03.rds"); beep(8) # because we have a data.table with a list, we better use rds instead of csv!
+
+## Getting sentence index of where match occurs
+df_window$temp_list <- apply(df_window, 1,function(x) {x$n_token_sentence-x$from_token_window}); beep(8)
+df_window$match_sentence_index <- as.numeric(lapply(df_window$temp_list, function(x) {which(x==min(x[x>=0]))})); beep(8)
+
+## Sentence before & after match 
+df_window$window_for_sent <- apply(df_window, 1, function(x) {
+  a <- 0
+  if(x$match_sentence_index>1){a <- c(a,-1)}
+  if(x$match_sentence_index<x$n_sentences){a <- c(a,+1)}
+  a <- sort(a)
+  x$window_sentences[x$match_sentence_index+a] %>% paste0(collapse=" ")
+}
+)
+
+df_window$window_noname <- df_window %>% apply(1,function(x){str_remove(x$window,x$pattern)})
+Sys.time()-start;beep(8)
+saveRDS(df_window, "df_windows_full_12-05_x.rds"); beep(10)
 
 # Sentiment Analyses ------------------------
 # 1. LSD Lexicoder Sentiment Dictionary
-print("1. Lexicoder Sentiment Analysis"); start <- Sys.time()
-sent_analysis <- dfm(corpus_window, dictionary=data_dictionary_LSD2015[1:4]) 
-df_window_13_sub <- cbind(df_window_13_sub, quanteda::convert(sent_analysis, to="data.frame"))
-df_window_13_sub$ntoken_window <- ntoken(df_window_13_sub$window_noname) 
-df_window_13_sub$weighted_lsd <- (df_window_13_sub$positive - df_window_13_sub$negative)/df_window_13_sub$ntoken_window
-df_window_13_sub$unweighted_lsd <- (df_window_13_sub$positive - df_window_13_sub$negative)
-df_window_13_sub$unweighted_log <- log((df_window_13_sub$positive + 0.5)/(df_window_13_sub$negative + 0.5))
-df_window_13_sub$weighted_log <- (log((df_window_13_sub$positive + 0.5)/(df_window_13_sub$negative + 0.5)))/df_window_13_sub$ntoken_window
-Sys.time()-start
+# print("1. Lexicoder Sentiment Analysis"); start <- Sys.time()
+# sent_analysis <- dfm(corpus_window, dictionary=data_dictionary_LSD2015[1:4]) 
+# df_window <- cbind(df_window, quanteda::convert(sent_analysis, to="data.frame"))
+# df_window$ntoken_window <- ntoken(df_window$window_noname) 
+# df_window$weighted_lsd <- (df_window$positive - df_window$negative)/df_window$ntoken_window
+# df_window$unweighted_lsd <- (df_window$positive - df_window$negative)
+# df_window$unweighted_log <- log((df_window$positive + 0.5)/(df_window$negative + 0.5))
+# df_window$weighted_log <- (log((df_window$positive + 0.5)/(df_window$negative + 0.5)))/df_window$ntoken_window
+# Sys.time()-start
 
 # 2. sentimentr
+# backup <- df_window
+# df_window <- readRDS("df_windows_full_12-05_x.rds")
 print("2. SentimentR Sentiment Analysis"); start <- Sys.time()
-sentences_sentr <- get_sentences(df_window_13_sub$window_noname)
-sent_r <- sentiment(sentences_sentr)
-df_window_13_sub$unweighted_sentr <- sent_r$sentiment
-df_window_13_sub$weighted_sentr <- sent_r$sentiment/df_window_13_sub$ntoken_window
-Sys.time()-start
+# sentences_sentr <- get_sentences(df_window$window_noname)
+
+sentences <- get_sentences(df_window$window_noname); beep(8)
+saveRDS(sentences, "sentences_noname_full.rds"); beep(10)
+df_window$unweighted_sentr <- sentiment_by(sentences)[,4]; beep(8)
+saveRDS(df_window, "df_windows_full_12-05_x1.rds");beep(10)
+df_window$ntoken_window <- quanteda::ntoken(df_window$window_noname)
+df_window$weighted_sentr <- df_window$unweighted_sentr/df_window$ntoken_window
+saveRDS(df_window, "df_windows_full_12-05_x2.rds");beep(10)
+# Sys.time()-start
+
+saveRDS(df_window, "df_windows_full_12-05_x2.rds");beep(10)
 
 # ranks
-df_window_13_sub <- df_window_13_sub %>% ungroup %>% 
+df_window <- df_window %>% ungroup %>% 
   mutate(
     percentrank_weighted_lsd = percent_rank(weighted_lsd),
     percentrank_unweighted_lsd = percent_rank(unweighted_lsd),
@@ -162,10 +213,10 @@ df_window_13_sub <- df_window_13_sub %>% ungroup %>%
     percentrank_weighted_log = percent_rank(weighted_log),
     percentrank_unweighted_sentr = percent_rank(unweighted_sentr),
     percentrank_weighted_sentr = percent_rank(weighted_sentr))
-fwrite(df_window_13_sub, "window_sub.csv")
+fwrite(df_window, "window_sub.csv")
 
 
-## Trashfile
+## Trashfile -----
 # # Use Stopwords?
 # stopp <- stopwords('english')
 # stopws <- stopp[!stopp %in% c("not", "very")]
@@ -176,12 +227,12 @@ fwrite(df_window_13_sub, "window_sub.csv")
 tempp <- fread("dail_full_nospeech.csv.gz")
 
 getXbeforeKWIC <- function(df,z){ 
-    from <- match("from",names(df)) # get index of "from" column
-    speech <- match("speech",names(df)) # get index of "from" column
-    apply(df, 1, function(x) {
-        if(as.numeric(x[from])<(z+1)) {a <- 0} else {a <- as.numeric(x[from])-1}
-        quanteda::tokens(x[speech])[[1]][(a-z):(a)]} %>% t %>% apply(1,paste,collapse=" ")
-    )}
+  from <- match("from",names(df)) # get index of "from" column
+  speech <- match("speech",names(df)) # get index of "from" column
+  apply(df, 1, function(x) {
+    if(as.numeric(x[from])<(z+1)) {a <- 0} else {a <- as.numeric(x[from])-1}
+    quanteda::tokens(x[speech])[[1]][(a-z):(a)]} %>% t %>% apply(1,paste,collapse=" ")
+  )}
 getXbeforeKWIC(tempp, 4)
 
 
@@ -217,3 +268,26 @@ tempp <- tempp %>% table(); beep(10)
 temppt <- tempp %>% sort(decreasing = T)
 temppt %>% head(400)
 temp2[grepl("\\. | \\! |\\?", temp2)]
+
+
+
+
+
+### TRASH -
+####
+df_window$randomslice <- rep(1:10000,(nrow(df_window)/10000) %>% ceiling)[1:nrow(df_window)] %>% sample()
+
+numCores<-detectCores(); cl <- makeCluster(numCores);registerDoSNOW(cl)
+# progress bar ------------------------------------------------------------
+iterations <- df_window$randomslice %>% max() 
+pb <- progress_bar$new(format = "iteration = :letter [:bar] :elapsed | eta: :eta",total = iterations,width = 60)
+progress_letter <- 1:1000
+progress <- function(n){pb$tick(tokens = list(letter = progress_letter[n]))} 
+opts <- list(progress = progress)
+#####
+start <- Sys.time()
+test <- foreach (i = 1:100,.combine = rbind,.packages = c('quanteda'),.verbose = T,.options.snow = opts
+) %dopar% {
+  ntoken(df_window$speech)
+};beep(8);Sys.time()-start;stopCluster(cl)
+####
