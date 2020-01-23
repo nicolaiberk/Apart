7310499a40843d5672eb82358ece1c891b7df33e
samunico <nicolai.berk@gmail.com>
2020-01-23 14:45:49 +0100

    update sampling.

--- strat_sample.R
+++ strat_sample.R
@@ -7,10 +7,18 @@
 #
 # 1. Preparation -----------------------------------------------------
 # __Loading Packages -------------------------------------------------
-setwd("~/GitHub/samunico/Apart/data")
 rm(list=ls())
 usePackage <- function(p) {if (!is.element(p, installed.packages()[,1]))install.packages(p,dep = TRUE, repos = "http://cran.wu.ac.at"); library(p, character.only = TRUE)}
 for (i in c('dplyr', 'beepr')){usePackage(i)}; rm(i, usePackage)
+
+# __preprocess full set------------------------------------------------
+setwd("C:/Users/samunico/Dropbox/Studium/Amsterdam/Studies/Semester 3/Block I/Internship/data")
+dta <- readRDS("df_windows_full_2020-01-22_x2.rds"); beep(10)
+dta_r <- dta %>% select(legper, keyword, window_for_sent, window_id)
+setwd("~/GitHub/samunico/Apart/data")
+write.csv(dta_r, "windows_df_230120.csv"); beep(10)
+
+
 
 # __global vars  -----------------------------------------------------
 prop <-  0.01 # proportion of original data to be sampled
@@ -31,5 +39,5 @@ sample <- df %>%
 data.table::fwrite(sample,"StratSample_01-23.csv"); beep(10)
 
 # figure eight sample
-f8_sample <- as.data.frame(sample)[, c('window_id', 'window', 'keyword')]
+f8_sample <- as.data.frame(sample)[, c('window_id', 'window_for_sent', 'keyword')]
 data.table::fwrite(f8_sample,"f8Val_StratSample_01-23.csv"); beep(10)
