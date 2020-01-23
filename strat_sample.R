6fc0dd3930b87fbdbba385acede7e74491e345be
samunico <nicolai.berk@gmail.com>
2020-01-23 16:43:44 +0100

    update stratified sampling.

--- strat_sample.R
+++ strat_sample.R
@@ -11,25 +11,24 @@ rm(list=ls())
 usePackage <- function(p) {if (!is.element(p, installed.packages()[,1]))install.packages(p,dep = TRUE, repos = "http://cran.wu.ac.at"); library(p, character.only = TRUE)}
 for (i in c('dplyr', 'beepr')){usePackage(i)}; rm(i, usePackage)
 
+# __global vars  -----------------------------------------------------
+prop <-  0.01 # proportion of original data to be sampled
+
 # __preprocess full set------------------------------------------------
 setwd("C:/Users/samunico/Dropbox/Studium/Amsterdam/Studies/Semester 3/Block I/Internship/data")
 dta <- readRDS("df_windows_full_2020-01-22_x2.rds"); beep(10)
 dta_r <- dta %>% select(legper, keyword, window_for_sent, window_id)
+rm(dta)
 setwd("~/GitHub/samunico/Apart/data")
 write.csv(dta_r, "windows_df_230120.csv"); beep(10)
 
 
 
-# __global vars  -----------------------------------------------------
-prop <-  0.01 # proportion of original data to be sampled
-
-# __Loading Data -----------------------------------------------------
-df <- data.table::fread("windows_df_230120.csv", encoding = "UTF-8", verbose = T); beep(10)
 
 
 # 2. Sampling --------------------------------------------------------
 set.seed(312) # ensures reproducability
-sample <- df %>% 
+sample <- dta_r %>% 
   group_by(legper) %>% 
   sample_frac(prop)
 
